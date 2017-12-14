497e5e35df2060a324b1a897c6a3b1bf7bcdd899
nirv44 <nicolas44.vivion@gmail.com>
2017-12-14 10:00:07 +0100

    add swapi get

--- back/module/gestion.js
+++ back/module/gestion.js
@@ -1,27 +1,12 @@
 "use strict";
 
-//var Client = require('node-rest-client').Client;
-//var client = new Client();
 
-//var hostswapi = "https://swapi.co/api";
+
+
+
+
 var data = require('./data.json');
 
 exports.peoples = function(req, res) {
 	res.json(data);
 }
-
-
-/*
-	var arg = {
-	 	headers: 
-		{ 
-			"Content-Type": "application/json"
-	 	}
-    };
-
-    client.get(hostswapi+"/people/", arg, function(data, response) {
-		res.json(data);
-	}).on('error', function(error) {
-		res.json({Title: false});
-	});
-*/
---
+++ back/module/swapi.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var Client = require('node-rest-client').Client;
+var client = new Client();
+
+var hostswapi = "https://swapi.co/api";
+
+exports.swapi = function(req, res) {
+	var arg = {
+	 	headers: 
+		{ 
+			"Content-Type": "application/json"
+	 	}
+    };
+
+    client.get(hostswapi+"/people/", arg, function(data, response) {
+		res.json(data);
+	}).on('error', function(error) {
+		res.json({Title: false});
+	});
+}
