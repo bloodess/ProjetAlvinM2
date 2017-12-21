fa66e4d7bf95df663e88950a7ae262c075ea698f
Bloodess <arnaud.champleboux@gmail.com>
2017-12-21 16:34:36 +0100

    fix some bug on data

--- back/module/googleapi.js
+++ back/module/googleapi.js
@@ -34,6 +34,7 @@ exports.addImage = function(req, res){
     var urlbyName = 'https://www.googleapis.com/customsearch/v1?key='+key+'&cx='+cx+'&searchType='+searchType+'&q='+element.name+' star wars'; // mettre le nom du personnage après le &q=
 
     client.get(urlbyName, arg, function(data, response) {
+      console.log('DATA : '+ data);
       nbarray = nbarray + 1;
       if(data !== undefined){
         var urlimg = data.items[0].link;
